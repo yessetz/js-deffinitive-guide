313b570d0856e559e110aade8e97a3c7d936217d
yessetz <yessetkhan@gmail.com>
2024-04-08 23:39:44 -0400

    feat: add new files

---
+++ strings.js
@@ -0,0 +1,7 @@
+console.log('Hello!!\b World');
+console.log('---------');
+console.log('Hello!!\v World');
+console.log('---------');
+console.log("Hello!!\f World");
+console.log('---------');
+console.log('Hello!!\0 World');
--- types.js
+++ types.js
@@ -1,1 +1,2 @@
-console.log('Hello World!');
+// Primitive types is Numbers, Strings, Boolean, Null, Undefined
+// All other types is Objects. Object is a collection of properties where each property has a name and a value.
